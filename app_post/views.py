2a6537ba7e1ba2b75f4a26b1563f4bf1ec1e1bf3
Piotr Górski <piotrkropagorski@gmail.com>
2024-05-11 14:49:08 +0200

    created basic layout without shitch sidebar

--- app_post/views.py
+++ app_post/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+import requests
 
 
 def home(request):
-    return render(request, 'home.html')
+    response = requests.get('https://randomuser.me/api/?results=35')
+    data = response.json()
+
+    people = []
+    if 'results' in data:
+        for user in data['results']:
+            first_name = user['name']['first']
+            last_name = user['name']['last']
+            username = user['login']['username']
+            picture = user['picture']['medium']
+            city = user['location']['city']
+            
+            people.append({
+                'first_name': first_name, 
+                'last_name': last_name,
+                'username': username,
+                'picture': picture,
+                'city': city
+                 })
+    else:
+        print('Nie udało się pobrać danych użytkowników')
+
+    return render(request, 'home.html', {'people': people})
